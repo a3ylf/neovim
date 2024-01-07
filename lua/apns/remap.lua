a428a9b38c76474a503a9c55abc4f45114ee5d16
a3ylf <alexandre3ylf@gmail.com>
2024-01-07 15:23:26 -0300

    harpoon and fugitive

--- lua/apns/remap.lua
+++ lua/apns/remap.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>bb", vim.cmd.Ex)
 
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
@@ -8,6 +8,26 @@ vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
 vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
-
 vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, {})
+
+vim.keymap.set("n", "<leader>gs", "<cmd>Git<cr>", { desc = "Git Status" })
+
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<C-w>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-e>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-y>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-i>", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
 
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -29,7 +29,7 @@ return {
         lazy = false,
         config = function()
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
-        
+
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({
                 capabilities = capabilities,
