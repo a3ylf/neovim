2583605245d14dc1fe060b1e9bea22262d54ec9d
a3ylf <alexandre3ylf@gmail.com>
2024-02-24 11:07:31 -0300

    Fixed interference with harpoon bindings

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -37,8 +37,8 @@ harpoon:setup()
 vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-w>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-e>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-e>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-f>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<C-y>", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<C-i>", function() harpoon:list():select(4) end)
 
