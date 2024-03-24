3c0c68e14e511e004f173f4f664435f0f356fe9f
a3ylf <alexandre3ylf@gmail.com>
2024-03-24 09:33:03 -0300

    NO BACKGROUND

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,10 +2,11 @@ return {
   {
     "tiagovla/tokyodark.nvim",
     opts = {
+      disable_background = true
     },
     config = function(_, opts)
         require("tokyodark").setup(opts) -- calling setup is optional
         vim.cmd [[colorscheme tokyodark]]
     end,
-}  -- ITS TOKYODARK AND NOT TOKYONIGHT IM SORRY
+}, -- ITS TOKYODARK AND NOT TOKYONIGHT IM SORRY 
 }
