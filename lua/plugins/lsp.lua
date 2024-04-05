35d0596b81cce77cec962fb966151b8048d1e425
apns <alexandre3ylf@gmail.com>
2024-04-05 19:34:22 -0300

    python

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,72 +1,71 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup({
-                ui = {
-                    border = "rounded",
-                    icons = {
-                        package_installed = "👍",
-                        package_pending = "😴",
-                        package_uninstalled = "👎",
-                    },
-                },
-            })
-        end,
-    },
-    {
-        'WhoIsSethDaniel/mason-tool-installer.nvim',
-    config = function()
-require('mason-tool-installer').setup({
-  -- Install these linters, formatters, debuggers automatically
-  ensure_installed = {
-    'black',
-    'mypy',
-    'pylint',
-    'stylua',
-      },
-    })
-        end,
-    },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({
+				ui = {
+					border = "rounded",
+					icons = {
+						package_installed = "👍",
+						package_pending = "😴",
+						package_uninstalled = "👎",
+					},
+				},
+			})
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"black",
+					"mypy",
+					"pylint",
+					"stylua",
+				},
+			})
+		end,
+	},
 
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "gopls","clangd","pyright"},
-            })
-        end,
-    },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "gopls", "clangd", "pyright" },
+			})
+		end,
+	},
 
-    {
-        "neovim/nvim-lspconfig",
-        event = {"BufReadPre","BufNewFile"},
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
 
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-                Lua = {
-                    diagnostics = {
-                        globals = { 'vim', 'lua_ls' },
-                        disable = {
-                            "lowercase-global",
-                        },
-                    },
-                },
-            })
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				Lua = {
+					diagnostics = {
+						globals = { "vim", "lua_ls" },
+						disable = {
+							"lowercase-global",
+						},
+					},
+				},
+			})
 
-            lspconfig.clangd.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.gopls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.pyright.setup({
-                capabilities = capabilities,
-            })
-            end,
-    },
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
+		end,
+	},
 }
