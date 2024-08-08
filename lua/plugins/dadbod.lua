aa4b82e9c4f4d55d906d96293f6085780d00dcc1
apns <alexandre3ylf@gmail.com>
2024-08-08 17:55:11 -0300

    No dadbod 😴

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -1,5 +1,7 @@
-return {
-  "tpope/vim-dadbod",
-  "kristijanhusak/vim-dadbod-completion",
-  "kristijanhusak/vim-dadbod-ui",
-}
+    return {
+  --"tpope/vim-dadbod",
+  --"kristijanhusak/vim-dadbod-completion",
+-- "kristijanhusak/vim-dadbod-ui",
+
+  --return
+    }
