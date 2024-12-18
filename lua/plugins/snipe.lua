e5b092aa7654889f9a8cc710ce0c6e7519c78918
a3ylf <alexandre3ylf@gmail.com>
2024-12-18 01:10:45 -0300

    sniping

---
+++ lua/plugins/snipe.lua
@@ -0,0 +1,26 @@
+return {
+    "leath-dub/snipe.nvim",
+    keys = {
+        {
+            "m",
+            function()
+                require("snipe").open_buffer_menu()
+            end,
+            desc = "Open Snipe buffer menu",
+        },
+    },
+    config = function()
+        local snipe = require("snipe")
+        snipe.setup({
+            hints = {
+                dictionary = "asdfglwertyuiop",
+            },
+            navigate = {
+                cancel_snipe = "m",
+                close_buffer = "q",
+                open_vsplit = "v",
+                open_split = "h",
+            },
+        })
+    end
+}
