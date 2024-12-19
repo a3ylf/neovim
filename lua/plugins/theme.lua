bdc7c55bb70623e336a176c8c738fa98552040ee
a3ylf <alexandre3ylf@gmail.com>
2024-12-18 23:13:05 -0300

    eldritch abomination

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,12 +1,16 @@
 return {
-{
-  "folke/tokyonight.nvim",
-  opts = {
-    transparent = true,
-    styles = {
-      sidebars = "transparent",
-      floats = "transparent",
-    },
-  },
-},
+	-- { "folke/tokyonight.nvim", opts = { transparent = true, styles = { sidebars = "transparent", floats = "transparent", }, }, },
+
+	{
+		"eldritch-theme/eldritch.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		},
+	},
 }
