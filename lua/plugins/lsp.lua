e6fd1c9fd7e6828c4007761884d13a84bfab63c9
a3ylf <alexandre3ylf@gmail.com>
2024-12-22 20:26:34 -0300

    dress better

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,87 +1,90 @@
 return {
-	{
-		"folke/lazydev.nvim",
-		ft = "lua",
-		opts = {
-			library = {
-				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-			},
-		},
-	},
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({
-				ui = {
-					border = "rounded",
-					icons = {
-						package_installed = "👍",
-						package_pending = "😴",
-						package_uninstalled = "👎",
-					},
-				},
-			})
-		end,
-	},
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		config = function()
-			require("mason-tool-installer").setup({
-				ensure_installed = {
-					"black",
-					"mypy",
-					"pylint",
-					"stylua",
-				},
-			})
-		end,
-	},
+    {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+            library = {
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            },
+        },
+    },
+    {
+        'eandrju/cellular-automaton.nvim'
+    },
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup({
+                ui = {
+                    border = "rounded",
+                    icons = {
+                        package_installed = "👍",
+                        package_pending = "😴",
+                        package_uninstalled = "👎",
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        config = function()
+            require("mason-tool-installer").setup({
+                ensure_installed = {
+                    "black",
+                    "mypy",
+                    "pylint",
+                    "stylua",
+                },
+            })
+        end,
+    },
 
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "gopls", "clangd", "pyright", "ts_ls" },
-			})
-		end,
-	},
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "gopls", "clangd", "pyright", "ts_ls" },
+            })
+        end,
+    },
 
-	{
-		"neovim/nvim-lspconfig",
-		event = { "BufReadPre", "BufNewFile" },
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
 
-		config = function()
-			-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local capabilities = require("blink.cmp").get_lsp_capabilities()
+        config = function()
+            -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-				Lua = {
-					diagnostics = {
-						globals = { "vim", "lua_ls" },
-						disable = {
-							"lowercase-global",
-						},
-					},
-				},
-			})
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim", "lua_ls" },
+                        disable = {
+                            "lowercase-global",
+                        },
+                    },
+                },
+            })
 
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.hls.setup({
-				capabilities = capabilities,
-			})
-		end,
-	},
+            lspconfig.clangd.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.gopls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.pyright.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.ts_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.hls.setup({
+                capabilities = capabilities,
+            })
+        end,
+    },
 }
