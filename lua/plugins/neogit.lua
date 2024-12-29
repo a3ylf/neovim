32331b423624ea4412ba739b5cb6b632468f129f
a3ylf <alexandre3ylf@gmail.com>
2024-12-29 02:52:03 -0300

    forgor neogit using telescope 🔭🔭

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -4,8 +4,10 @@ return {
 		"nvim-lua/plenary.nvim", -- required
 		"sindrets/diffview.nvim", -- optional - Diff integration
 
-		-- Only one of these is needed.
-		"nvim-telescope/telescope.nvim", -- optional
+    -- Only one of these is needed.
+    -- "nvim-telescope/telescope.nvim", -- optional
+    "ibhagwan/fzf-lua",              -- optional
+    -- "echasnovski/mini.pick",         -- optional
 	},
 	config = true,
 }
