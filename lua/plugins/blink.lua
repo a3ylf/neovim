147cbe27bac3d356e73bdb03c9ba6627120c47fb
a3ylf <alexandre3ylf@gmail.com>
2024-12-29 03:02:31 -0300

    auto show definitions

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -23,7 +23,7 @@ return {
             },
             documentation = {
                 auto_show = true,
-                auto_show_delay_ms = 50,
+                auto_show_delay_ms = 0,
             },
             ghost_text = { enabled = true },
         },
