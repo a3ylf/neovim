32e578ec52ed56a5febbebdcb4158b279354bf7a
a3ylf <alexandre3ylf@gmail.com>
2024-12-30 03:38:45 -0300

    Grug

--- init.lua
+++ init.lua
@@ -1,16 +1,17 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = ","
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", 
-    lazypath,
-  })
-end	
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
@@ -19,4 +20,4 @@ require("core")
 
 vim.diagnostic.config({ virtual_text = false })
 
-vim.cmd[[colorscheme eldritch]]
+vim.cmd([[colorscheme eldritch]])
